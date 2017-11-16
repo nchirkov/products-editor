fc6d029ae460974b3b430fb552898b19658c39cb
nchirkov <chirkov.nikita@gmail.com>
2017-11-16 22:59:34 +0300

    Add index html template

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Products Editor</title>
+    <meta name="description" content="Simple Products Editor">
+ </head>
+
+<body>
+    Hello, world!
+</body>
+</html>
