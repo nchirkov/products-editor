4dd289925a07ef5bd21158830d2c6cc884d9bc98
nchirkov <chirkov.nikita@gmail.com>
2017-11-18 19:23:02 +0300

    Fix renaming error

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'config.php';
-    require_once 'database.php';
+    require_once 'repository.php';
     initProducts();
 
     require_once config('template_path').'/home.php';
